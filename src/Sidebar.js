c9acbef0e7789fa07c48d18d1e1ddb1b8b5fceef
Stephanie Raymos <58127737+stephanieraymos@users.noreply.github.com>
2021-03-02 11:38:12 -0800

    Modal opening and closing

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -2,6 +2,7 @@ import React from "react";
 import logo from "./logo.svg";
 import { FaTimes } from "react-icons/fa";
 import { social, links } from "./data";
+import { useGlobalContext } from "./context";
 
 const Sidebar = () => {
   return (
