7a835a1372ce7d09e79f2d36a25b456e51d5e1f6
Stephanie Raymos <58127737+stephanieraymos@users.noreply.github.com>
2021-03-02 10:57:18 -0800

    Links + social icons displaying in sidebar

--- src/Modal.js
+++ src/Modal.js
@@ -2,7 +2,7 @@ import React from "react";
 import { FaTimes } from "react-icons/fa";
 const Modal = () => {
   return (
-    <div className={`modal-overlay show-modal`}>
+    <div className={`modal-overlay`}>
       <div className="modal-container">
         <h3>Modal Content</h3>
         <button className="close-modal-btn">
