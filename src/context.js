fa939c15c302b49cf73c059ed1fffd16e0c60ec2
Stephanie Raymos <58127737+stephanieraymos@users.noreply.github.com>
2021-03-02 11:08:11 -0800

    useCntext

--- src/context.js
+++ src/context.js
@@ -1,1 +1,12 @@
 import React, { useState, useContext } from "react";
+
+const AppContext = React.createContext();
+
+const AppProvider = ({ children }) => {
+  //Wrapping whole app in Provider
+  return <AppContext.Provider value="Hello">{children}</AppContext.Provider>;
+};
+
+// Custom hook
+
+export { AppContext, AppProvider };
