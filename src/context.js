d513aa3781ab3355fe6aa637a1e740ebdbf10e61
Stephanie Raymos <58127737+stephanieraymos@users.noreply.github.com>
2021-03-02 11:29:33 -0800

    Passing all values to the AppContext

--- src/context.js
+++ src/context.js
@@ -4,9 +4,44 @@ const AppContext = React.createContext();
 
 const AppProvider = ({ children }) => {
   //Wrapping whole app in Provider
-  return <AppContext.Provider value="Hello">{children}</AppContext.Provider>;
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const openSidebar = () => {
+    setIsSidebarOpen(true);
+  };
+
+  const closeSidebar = () => {
+    setIsSidebarOpen(false);
+  };
+
+  const openModal = () => {
+    setIsModalOpen(true);
+  };
+
+  const closeModal = () => {
+    setIsModalOpen(false);
+  };
+
+  return (
+    <AppContext.Provider
+      value={{
+        isModalOpen,
+        isSidebarOpen,
+        openModal,
+        openSidebar,
+        closeModal,
+        closeSidebar,
+      }}
+    >
+      {children}
+    </AppContext.Provider>
+  );
 };
 
 // Custom hook
+export const useGlobalContext = () => {
+  return useContext(AppContext);
+};
 
 export { AppContext, AppProvider };
