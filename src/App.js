ecc8f80a2c901438b24410fa191a4a390b4b6a64
Stephanie Raymos <58127737+stephanieraymos@users.noreply.github.com>
2021-03-02 10:19:35 -0800

    Base plate

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Modal from "./Modal";
+import Sidebar from "./Sidebar";
+import Home from "./Home";
+function App() {
+  return (
+    <>
+      <h2>modal-sidebar</h2>
+    </>
+  );
+}
+
+export default App;
